cef7ae4a06755dc2ea7b81e7816c58c820ff0688
Ömer Fırat <huseyinomerfirat@condolife.co>
2024-09-29 11:50:09 +0300

    Initial Commit

---
+++ FordCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FordCase
+//
+//  Created by Mac on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
